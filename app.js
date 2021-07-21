011c361416e930074afc75b48b61e2cbfe32e48f
ddizi <ruroo@naver.com>
2021-07-21 16:42:39 +0900

    1

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 var express = require('express');
+
+const cors = require('cors');
+
 var app = express();
 
+app.use(cors()); // CORS 미들웨어 추가
 
 //api 서비스를 할 배열
 const contents = [
     {
         id:1,
-        name:'손기섭'
+        name:'HTML 공부하기',
+        done:true
     },
     {
         id:2,
-        name:'권회준'
-    }
+        name:'JS 공부하기',
+        done:true
+    },
+    {
+        id:3,
+        name:'CSS 공부하기',
+        done:false
+    },
 ]
 
 // respond with "hello world" when a GET request is made to the homepage
@@ -22,4 +33,4 @@ app.get('/', function(req, res) {
 
 
 //express 웹서버 리스닝
-app.listen(5000, () => console.log(`Listening on port 5000`));
+app.listen(3000, () => console.log(`Listening on port 5000`));
