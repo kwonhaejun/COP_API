2c5b2ed12b570ec8336fff79e19d6603d172a849
ddizi <ruroo@naver.com>
2021-09-14 16:33:43 +0900

    todo modi

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors()); // CORS 미들웨어 추가
 const contents = [
     {
         id:1,
-        name:'HTML 공부하기',
+        name:'HTML 공부하기 시러',
         done:true
     },
     {
@@ -23,6 +23,11 @@ const contents = [
         name:'CSS 공부하기',
         done:false
     },
+    {
+        id:4,
+        name:'angular 공부하기',
+        done:false
+    },
 ]
 
 // respond with "hello world" when a GET request is made to the homepage
@@ -31,6 +36,13 @@ app.get('/', function(req, res) {
   res.json(contents);
 });
 
+//todo 등록
+app.get('/add', function(req, res) {
+    // url이 http://a.com/topic?id=1&name=siwa 일때
+    contents.push({id:contents.length+1,name:req.query.name,done:false});
+    //res.send(req.query.id+','+req.query.name); // 1,siwa 출력
+    res.json(contents);
+  })
 
 //express 웹서버 리스닝
-app.listen(3000, () => console.log(`Listening on port 5000`));
+app.listen(3000, () => console.log(`Listening on port 3000`));
